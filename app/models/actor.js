551013d5fbdbe2ee34f0da24fd72f25d6feb01a5
Philip Duffy <willmanduffy@gmail.com>
2014-12-08 10:32:13 -0500

    Properly set age attribute type on Actor model

--- app/models/actor.js
+++ app/models/actor.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  age: DS.attr(),
+  age: DS.attr('number'),
 
   nameAndAge: function() {
     return this.get('name') + ' (Age: ' + this.get('age') + ')';
